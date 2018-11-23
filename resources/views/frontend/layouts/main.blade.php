0dd0279f8d6e459364647da2daa6d63bb67c624b
karthus <1426921812@qq.com>
2018-11-23 10:47:34 +0800

    adjust main.html css

--- resources/views/frontend/layouts/main.blade.php
+++ resources/views/frontend/layouts/main.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @section('css')
-        <link rel="stylesheet" href="{{ asset('css/light7.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/light7.min.css') }}">
     @show
     <title>自玩多元化</title>
 </head>
